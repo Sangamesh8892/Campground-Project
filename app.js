33bc8cce04df964068058da80e3688691e9327b8
sangamesh <demo@example.com>
2025-02-07 18:50:44 +0530

    app.js updated to correct the landing page

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use((req,res,next)=>{
     res.locals.error = req.flash('error');
     next()
 })
-app.get('/home', (req,res)=>{
+app.get('/', (req,res)=>{
     res.render('home.ejs')
 })
 app.use('/',userRoutes)
